7826aac5123525d467b7f6ed41eaa7384c0cd56f
Rishabh shukla <rishabhshuklayt@gmail.com>
2025-04-23 22:07:02 +0530

    First Commit

---
+++ Frontend-NQR/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+  <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 space-y-4">
+      <h1 className="text-3xl font-bold mb-6">Welcome</h1>
+      
+      <button className="px-6 py-3 bg-blue-600 text-white rounded-2xl hover:bg-blue-700 transition">Home</button>
+      <button className="px-6 py-3 bg-green-600 text-white rounded-2xl hover:bg-green-700 transition">Profile</button>
+      <button className="px-6 py-3 bg-yellow-500 text-white rounded-2xl hover:bg-yellow-600 transition">Cart</button>
+      <button className="px-6 py-3 bg-red-600 text-white rounded-2xl hover:bg-red-700 transition">Admin Login</button>
+      <button className="px-6 py-3 bg-purple-600 text-white rounded-2xl hover:bg-purple-700 transition">Contact</button>
+    </div>
+    </>
+  )
+}
+
+export default App
