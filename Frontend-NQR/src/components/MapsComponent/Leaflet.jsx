271c1499c7b497e59c366e84f5b58119b09e2872
Rishabh shukla <rishabhshuklayt@gmail.com>
2025-04-26 11:16:00 +0530

    Update Leaflet component to include zIndex style for improved map layering.

--- Frontend-NQR/src/components/MapsComponent/Leaflet.jsx
+++ Frontend-NQR/src/components/MapsComponent/Leaflet.jsx
@@ -17,7 +17,7 @@ L.Icon.Default.mergeOptions({
 
 const MyMap = () => {
   return (
-    <MapContainer center={[28.6139, 77.2090]} zoom={13} style={{ height: "100vh", width: "100%" }}>
+    <MapContainer center={[28.6139, 77.2090]} zoom={13} style={{ height: "100vh", width: "100%", zIndex: 1  }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='© <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
