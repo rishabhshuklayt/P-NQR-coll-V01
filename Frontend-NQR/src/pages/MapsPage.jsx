99854db6683be831824c4c9ac361736561c57f54
Rishabh shukla <rishabhshuklayt@gmail.com>
2025-04-26 11:40:38 +0530

    Add MapSearchBar component for location search functionality; update MapsPage layout to position map and search bar correctly.

--- Frontend-NQR/src/pages/MapsPage.jsx
+++ Frontend-NQR/src/pages/MapsPage.jsx
@@ -1,19 +1,43 @@
 import { useState } from 'react';
-import TopBar from '../components/DynamicComponents/TopBar';
+
 import BottomNavBar from '../components/BottomNavBar';
 import MyMap from '../components/MapsComponent/Leaflet';
+import MapSearchBar from '../components/MapsComponent/MapSearchBar';
+
+// const MapsPage = () => {
+//   return (
+//     <div className="flex flex-col min-h-screen">
+      
+//       <MapSearchBar />
+      
+//       <MyMap />
+      
+//       <BottomNavBar />
+//     </div>
+//   );
+// };
+
+// export default MapsPage; 
 
 const MapsPage = () => {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <TopBar restaurantName="Store Locator" tagline="Find stores near you" />
-      
-      
-      <MyMap />
-      
-      <BottomNavBar />
-    </div>
-  );
-};
-
-export default MapsPage; 
+    return (
+      <div className="relative w-screen h-screen overflow-hidden">
+        
+        {/* Map should be behind everything */}
+        <div className="absolute inset-0 z-0">
+          <MyMap />
+        </div>
+  
+        {/* Search bar floats above */}
+        <div className="absolute top-4 left-1/2 transform -translate-x-1/2 z-10 w-11/12 max-w-md">
+          <MapSearchBar />
+        </div>
+  
+        {/* Bottom nav at the bottom */}
+        <div className="absolute bottom-0 left-0 w-full z-10">
+          <BottomNavBar />
+        </div>
+      </div>
+    );
+  };
+  export default MapsPage; 
